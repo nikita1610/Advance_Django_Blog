5e88f6e438554cd37e389c70634e197e844e8f50
nikita jain <jainnikita381@gmail.com>
2020-10-23 19:22:06 +0530

    Day1

---
+++ posts/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+
+from .models import Post
+
+class PostForm(forms.ModelForm):
+	publish=forms.DateField(widget=forms.SelectDateWidget)
+	class Meta:
+		model=Post
+		fields=[
+			"title",
+			"content",
+			"image",
+			"draft",
+			"publish"
+		]
